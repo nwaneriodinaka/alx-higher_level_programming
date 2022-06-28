8bc295f9909157184e4c08f38f07493fd0e2278c
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-06-28 10:03:34 -0700

    finding love

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 import random
 number = random.randint(-10000, 10000)
 if (number > 0):
@@ -8,6 +7,3 @@ elif (number == 0):
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
-=======
-
->>>>>>> 23cd1b558fc5779c79dc2a19b851a61142c99d68
