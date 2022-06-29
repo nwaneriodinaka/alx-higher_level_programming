c8dbf22bf20d4863ebd79dbfa9a8795971c73504
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-06-28 18:34:43 -0700

    love

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    for {x} < 10:
-        print(f"0")
-    print(f"{x}", end = ', ')
+    if x == 99:
+        print("{}" .format(x))
+    else:
+        print("{:02d}" .format(x), end=', ')
