ee213b05da013e61229f56062dc8db0ed4a86137
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-07-04 11:05:10 -0700

    likeness

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(0, len(x)):
-            print("{:d} ".format(x[y]), end='')
-        print()
+            if y < (len(x)) - 1:
+                print("{:d}".format(x[y]), end=' ')
+            else:
+                print("{:d}".format(x[y]))
