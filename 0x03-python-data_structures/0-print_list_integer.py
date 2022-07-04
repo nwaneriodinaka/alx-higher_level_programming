a5ed0def5ce3f164beb7374c1cb56f793f656f9d
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-07-04 07:28:34 -0700

    joy

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/urs/bin/python3
+#if __name__ == "__main__":
+def print_list_integer(my_list=[]):
+        n = len(my_list)
+        for x in range(n):
+            print("{}".format(my_list[x]))
