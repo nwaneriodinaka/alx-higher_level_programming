143e75c9603000efe3017211147f886191af69d8
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-07-04 10:58:49 -0700

    trenches

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(0, len(x)):
-            print("{:d}".format(x[y]), end=' ')
+            print("{:d} ".format(x[y]), end='')
         print()
