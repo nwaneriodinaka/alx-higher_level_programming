fbfcca57ca7486e70d9d45d4948f13e374a696de
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-07-07 02:55:07 -0700

    keep pushing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ if __name__ == "__main__":
     import sys
 
     argc = len(sys.argv)
-    if argc > 1:
-        print("{} argument:".format(argc - 1))
-        for x in range(1, argc):
-            print("{}: {}".format(x, sys.argv[x]))
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    elif argc > 2:
+        print("{} arguments:".format(argc - 1))
     else:
-        print("{} arguments.".format(argc-1))
+        print("{} argument:".format(argc - 1))
+    for x in range(1, argc):
+        print("{}: {}".format(x, sys.argv[x]))
